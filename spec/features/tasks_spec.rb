1c6c28bb4f062e93fb4d324bb52100f6c783e94f
有吉　貴紀 <>
2020-12-02 18:02:09 +0900

    処理の切り出し、リファクタについて学習完了

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,24 +1,47 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, name: "Rspec tutorial",owner: user) }
+
+   let!(:task) { project.tasks.create!(name: "Finish Rspec tutorial") }
+
   scenario "ユーザーがタスクの状態を切り替える", js: true do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, name: "Rspec tutorial",owner: user)
-    task = project.tasks.create!(name: "Finish Rspec tutorial")
 
-    visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
-    click_link "Rspec tutorial"
-    check "Finish Rspec tutorial"
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to be_completed
+    sign_in user
+    go_to_project "Rspec tutorial"
+    complete_task "Finish Rspec tutorial"
+    expect_complete_task "Finish Rspec tutorial"
+    undo_complete_task "Finish Rspec tutorial"
+    expect_incomplete_task "Finish Rspec tutorial"
 
-    uncheck "Finish Rspec tutorial"
-
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to_not be_completed
   end
+
+  def go_to_project(name)
+    visit root_path
+    click_link name
+  end
+
+  def complete_task(name)
+    check name
+  end
+
+  def expect_complete_task(name)
+    aggregate_failures do
+      expect(page).to have_css "label.completed",text: name
+      expect(task.reload).to be_completed
+    end
+  end
+
+  def undo_complete_task(project)
+    uncheck project
+  end
+
+  def expect_incomplete_task(name)
+    aggregate_failures do
+      expect(page).to have_css "label.completed",text: name
+      expect(task.reload).to_not be_completed
+    end
+  end
+
 end
